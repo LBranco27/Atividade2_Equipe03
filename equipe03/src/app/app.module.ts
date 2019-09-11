b941735744380eef3dcb99a77ffb6faa2e7251e5
pedronso <pedrocaetanoaraujopc@gmail.com>
2019-09-11 08:25:15 -0300

    Pedro

--- equipe03/src/app/app.module.ts
+++ equipe03/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PredoComponent } from './predo/predo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PredoComponent
   ],
   imports: [
     BrowserModule
---
+++ equipe03/src/app/predo/predo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-predo',
+  templateUrl: './predo.component.html',
+  styleUrls: ['./predo.component.css']
+})
+export class PredoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
